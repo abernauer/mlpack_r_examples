f01e3ef58f0eb944e285b4d105f08dff3e79a965
Andrew Bernauer <bernaue2@illinois.edu>
2020-10-09 10:47:21 -0500

    clarified some comments

--- cfandwordcloud.R
+++ cfandwordcloud.R
@@ -1,3 +1,4 @@
+# load required libraries for example
 library(mlpack)
 library(data.table)
 library(wordcloud)
@@ -10,8 +11,10 @@ library(ggplot2)
 ratings <- fread("http://www.mlpack.org/datasets/ml-20m/ratings-only.csv.gz")
 movies <- fread("http://www.mlpack.org/datasets/ml-20m/movies.csv.gz")
 
+# check if the data sets have any missing values
 any(is.na(ratings))
 any(is.na(movies))
+
 # display first 10 rows of the ratings data.
 head(ratings, n = 10)
 
@@ -19,9 +22,9 @@ head(ratings, n = 10)
 # display first 10 rows of the movies data.
 head(movies, n = 10)
 
-# clean this code up with the pipe operator, %>%, also read apply function and then
+# clean this code up with the pipe operator, %>%, read as genres_vec %>% table() is equivalent to table(genres_vec)
 # create corpus of the movie titles
-# clean the corpus movie object, removing stopwords, 
+# clean the corpus movie object, removing stopwords, remove numbers
 
 movie_corpus <- Corpus(VectorSource(movies$title))
 
@@ -64,9 +67,9 @@ pal <- brewer.pal(6, "BuGn")
 genre_word_cloud <- wordcloud(genres_vec, scale = c(3, .5), max.words = 25, random.order = FALSE, random.color = FALSE, colors = pal)
 
 # create a table of the genres then call the function prop.table on the object
-# 
-table_of_genres <- table(genres_vec) %>%
-  prop.table()
+table_of_genres <- genres_vec %>%
+table() %>% 
+prop.table()
 
 # return the frequency or percentage of each film genre.
 table_of_genres
@@ -85,7 +88,7 @@ ratings_hist
 
 # the mean rating is 3.5 whith 3 and 4 being the other most common moving ratings.
 # hold out %10 percent of the dataset into a test set
-# Set seet for reproducibility 
+# Set seed for reproducibility 
 set.seed(387)
 output <- ratings   %>%
        as.matrix(.) %>%
